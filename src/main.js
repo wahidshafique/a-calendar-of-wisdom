64b65387ec6d2fe6322d9091a1bf764f5f3d39ab
Wahid Shafique <wahidshafique@hotmail.com>
2020-03-18 08:42:32 -0400

    change casing for file import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "@babel/polyfill";
-import App from "./App.svelte";
+import App from "./app.svelte";
 import quotes from "./data/quotes.json";
 const app = new App({
   target: document.getElementById("main"),
